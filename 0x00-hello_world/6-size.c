9dc0e22b68221a6c2625b50fad04c6b4de6e22fc
elgrgaway <elgrgaway@gmail.com>
2023-03-20 13:43:51 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main()
+/*
+ * main - main function
+ * return:always return 0
+ */
+int main(void)
 {
 	printf("Size of a char: %lu",sizeof(char));
 	printf("Size of a int: %lu",sizeof(int));
