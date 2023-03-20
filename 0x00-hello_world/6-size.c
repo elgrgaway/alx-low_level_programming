2052e71d55ecf3723a49324d70f41b14138fa891
elgrgaway <elgrgaway@gmail.com>
2023-03-20 13:37:16 -0700

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Size of a char: %d",sizeof(char));
+	printf("Size of a int: %d",sizeof(int));
+	printf("Size of a long int: %d",sizeof(long int));
+	printf("Size of a long long int: %d",sizeof(long long int));
+	printf("Size of a float: %d",sizeof(float));
+	retutn 0;
+}
