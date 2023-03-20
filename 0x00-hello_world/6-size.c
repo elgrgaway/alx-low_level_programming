79e2122ec81acc8c7a64619d0395fb101e5935eb
elgrgaway <elgrgaway@gmail.com>
2023-03-20 13:45:05 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu",sizeof(char));
-	printf("Size of a int: %lu",sizeof(int));
-	printf("Size of a long int: %lu",sizeof(long int));
-	printf("Size of a long long int: %lu",sizeof(long long int));
-	printf("Size of a float: %lu",sizeof(float));
+	printf("Size of a char: %lu \n",sizeof(char));
+	printf("Size of a int: %lu \n",sizeof(int));
+	printf("Size of a long int: %lu \n",sizeof(long int));
+	printf("Size of a long long int: %lu \n",sizeof(long long int));
+	printf("Size of a float: %lu \n",sizeof(float));
 	retutn 0;
 }
